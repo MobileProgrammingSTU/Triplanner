5e613de4dfa43aea4c1e5ebabffaef798aac2b67
ninanoray <77103814+ninanoray@users.noreply.github.com>
2022-10-05 18:22:44 +0900

    디자인 적용 동작은 그대로

--- app/src/main/java/com/seoultech/triplanner/PlannerThirdActivity.java
+++ app/src/main/java/com/seoultech/triplanner/PlannerThirdActivity.java
@@ -3,43 +3,37 @@ package com.seoultech.triplanner;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class PlannerThirdActivity extends AppCompatActivity {
 
-    LinearLayout linearAttraction, linearRestaurant, linearCafe;
-
     Toolbar toolBar;
 
     ImageView cafeView1;
     TextView cafeText1, cafeTag1;
+    ImageButton btnAttraction;
+    ImageButton btnRestaurant;
+    ImageButton btnCafe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.planner_third);
 
-        toolBar = findViewById(R.id.toolBar);
-        setSupportActionBar (toolBar);                  //액티비티의 앱바(App Bar)로 지정
-        ActionBar actionBar = getSupportActionBar ();   //앱바 제어를 위해 툴바 액세스
-        actionBar.setDisplayHomeAsUpEnabled (true);     // 앱바에 뒤로가기 버튼 만들기
-        actionBar.setTitle("플랜 추가");
-
-        linearAttraction = (LinearLayout) findViewById(R.id.linearAttraction);
-        linearRestaurant = (LinearLayout) findViewById(R.id.linearRestaurant);
-        linearCafe = (LinearLayout) findViewById(R.id.linearCafe);
+        btnAttraction = (ImageButton) findViewById(R.id.imageAtt);
+        btnRestaurant = (ImageButton) findViewById(R.id.imageRes);
+        btnCafe = (ImageButton) findViewById(R.id.imageCafe);
         cafeView1 = (ImageView) findViewById(R.id.cafeView1);
         cafeText1 = (TextView) findViewById(R.id.cafeText1);
         cafeTag1 = (TextView) findViewById(R.id.cafeTag1);
 
-        linearAttraction.setOnClickListener(new View.OnClickListener() {
+        btnAttraction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 cafeView1.setVisibility(View.INVISIBLE);
@@ -47,7 +41,7 @@ public class PlannerThirdActivity extends AppCompatActivity {
                 cafeTag1.setVisibility(View.INVISIBLE);
             }
         });
-        linearRestaurant.setOnClickListener(new View.OnClickListener() {
+        btnRestaurant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 cafeView1.setVisibility(View.INVISIBLE);
@@ -55,7 +49,7 @@ public class PlannerThirdActivity extends AppCompatActivity {
                 cafeTag1.setVisibility(View.INVISIBLE);
             }
         });
-        linearCafe.setOnClickListener(new View.OnClickListener() {
+        btnCafe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 cafeView1.setVisibility(View.VISIBLE);
