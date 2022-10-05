fe7dc41a313a7ca41eada1a962f7e7f1ca62e5ed
ninanoray <77103814+ninanoray@users.noreply.github.com>
2022-10-06 03:56:46 +0900

    활동버튼 디자인 수정, 맛집과 명소 배너 추가, 다음버튼 추가

--- app/src/main/java/com/seoultech/triplanner/PlannerThirdActivity.java
+++ app/src/main/java/com/seoultech/triplanner/PlannerThirdActivity.java
@@ -1,46 +1,44 @@
 package com.seoultech.triplanner;
 
+import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlannerThirdActivity extends AppCompatActivity {
 
-    ImageView cafeView1;
-    TextView cafeText1, cafeTag1;
-    ImageView btnAttraction, btnRestaurant, btnCafe;
+    RelativeLayout att1, rest1, cafe1;
+    Button btnAttraction, btnRestaurant, btnCafe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.planner_third);
 
-        btnAttraction = (ImageView) findViewById(R.id.imageAtt);
-        btnRestaurant = (ImageView) findViewById(R.id.imageRes);
-        btnCafe = (ImageView) findViewById(R.id.imageCafe);
-        cafeView1 = (ImageView) findViewById(R.id.cafeView1);
-        cafeText1 = (TextView) findViewById(R.id.cafeText1);
-        cafeTag1 = (TextView) findViewById(R.id.cafeTag1);
+        btnAttraction = (Button) findViewById(R.id.imageAtt);
+        btnRestaurant = (Button) findViewById(R.id.imageRes);
+        btnCafe = (Button) findViewById(R.id.imageCafe);
+
+        att1 = (RelativeLayout) findViewById(R.id.att1);
+        rest1 = (RelativeLayout) findViewById(R.id.rest1);
+        cafe1 = (RelativeLayout) findViewById(R.id.cafe1);
 
         btnAttraction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(btnAttraction.isSelected()){
-                    btnAttraction.clearColorFilter();
+                    btnAttraction.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#404040")));
+                    att1.setVisibility(View.GONE);
                 }
                 else {
-                    btnAttraction.setColorFilter(Color.parseColor("#FFD37A"), PorterDuff.Mode.SRC_IN);
-
-                    cafeView1.setVisibility(View.GONE);
-                    cafeText1.setVisibility(View.GONE);
-                    cafeTag1.setVisibility(View.GONE);
+                    btnAttraction.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFD37A")));
+                    att1.setVisibility(View.VISIBLE);
                 }
                 btnAttraction.setSelected(!btnAttraction.isSelected());
             }
@@ -49,14 +47,12 @@ public class PlannerThirdActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(btnRestaurant.isSelected()){
-                    btnRestaurant.clearColorFilter();
+                    btnRestaurant.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#404040")));
+                    rest1.setVisibility(View.GONE);
                 }
                 else {
-                    btnRestaurant.setColorFilter(Color.parseColor("#F26C73"), PorterDuff.Mode.SRC_IN);
-
-                    cafeView1.setVisibility(View.GONE);
-                    cafeText1.setVisibility(View.GONE);
-                    cafeTag1.setVisibility(View.GONE);
+                    btnRestaurant.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#F26C73")));
+                    rest1.setVisibility(View.VISIBLE);
                 }
                 btnRestaurant.setSelected(!btnRestaurant.isSelected());
             }
@@ -65,18 +61,12 @@ public class PlannerThirdActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(btnCafe.isSelected()){
-                    btnCafe.clearColorFilter();
-
-                    cafeView1.setVisibility(View.GONE);
-                    cafeText1.setVisibility(View.GONE);
-                    cafeTag1.setVisibility(View.GONE);
+                    btnCafe.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#404040")));
+                    cafe1.setVisibility(View.GONE);
                 }
                 else {
-                    btnCafe.setColorFilter(Color.parseColor("#6CE4B4"), PorterDuff.Mode.SRC_IN);
-
-                    cafeView1.setVisibility(View.VISIBLE);
-                    cafeText1.setVisibility(View.VISIBLE);
-                    cafeTag1.setVisibility(View.VISIBLE);
+                    btnCafe.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#6CE4B4")));
+                    cafe1.setVisibility(View.VISIBLE);
                 }
                 btnCafe.setSelected(!btnCafe.isSelected());
             }
