0d506170f552888c0ea7d2454b0f7fe2a2451407
Moojun <mgim32137@gmail.com>
2022-10-12 03:20:32 +0900

    마지막 화면 임의 구현

---
+++ app/src/main/java/com/seoultech/triplanner/FinishPlanner.java
@@ -0,0 +1,14 @@
+package com.seoultech.triplanner;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FinishPlanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.finish);
+    }
+}
