97a5015f8be9f1f6a0541a2a4c0310e1b0a72da5
Moojun <mgim32137@gmail.com>
2022-10-14 14:27:12 +0900

    Feat: Finish 배경 수정 및 Home 버튼 기능 구현

--- app/src/main/java/com/seoultech/triplanner/FinishPlanner.java
+++ app/src/main/java/com/seoultech/triplanner/FinishPlanner.java
@@ -1,20 +1,39 @@
 package com.seoultech.triplanner;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.seoultech.triplanner.PlaceIntent;
-
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Set;
 
 public class FinishPlanner extends AppCompatActivity {
 
+    ImageButton FooterBtnHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.finish);
+
+        FooterBtnHome = (ImageButton) findViewById(R.id.FooterBtnHome);
+        FooterBtnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // 메인 화면으로 가기 전, 아래 내역들 초기화 필요
+                PlaceIntent.savedDateMap = new LinkedHashMap<>();
+                PlaceIntent.placeIntent = new Intent();
+                PlaceIntent.savedPlacesMap = new LinkedHashMap<>();
+
+                Intent intentHome = new Intent(FinishPlanner.this, MainActivity.class);
+                startActivity(intentHome);
+            }
+        });
 
         Set<Integer> keySet = PlaceIntent.savedPlacesMap.keySet();
         for (int i : keySet) {
