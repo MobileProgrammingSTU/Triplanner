98f076d9184e1d0b132941b8750a4e8e1d4fee7b
Moojun <mgim32137@gmail.com>
2022-11-01 17:01:36 +0900

    User 정보를 담을 Java Bean 객체 생성

---
+++ app/src/main/java/com/seoultech/triplanner/UserAccount.java
@@ -0,0 +1,38 @@
+package com.seoultech.triplanner;
+
+/*
+   사용자 계정 정보 모델 클래스
+ */
+public class UserAccount {
+
+    private String emailId;     // email 아이디
+    private String passwd;      // 비밀번호
+    private String idToken;     // Firebase Uid (고유 토큰 정)
+
+    // Firebase 의 경우 빈 생성자를 만들지 않으면 데이터를 조회할 때 오류가 난다고 한다.
+    public UserAccount() {}
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+}
