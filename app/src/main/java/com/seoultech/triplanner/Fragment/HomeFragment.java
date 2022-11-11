b6a178498e13e2cd32dd5d71cc3951b191f2dce6
ninanoray <77103814+ninanoray@users.noreply.github.com>
2022-11-12 03:55:35 +0900

    연동 성공! glide문제가 아니었음. 키와 객체 프로퍼티 연동문제였음

--- app/src/main/java/com/seoultech/triplanner/Fragment/HomeFragment.java
+++ app/src/main/java/com/seoultech/triplanner/Fragment/HomeFragment.java
@@ -15,7 +15,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.seoultech.triplanner.Model.PostItem;
 import com.seoultech.triplanner.PostAdapter;
@@ -102,4 +101,5 @@ public class HomeFragment extends Fragment {
             }
         });
     }
+
 }
--- app/src/main/java/com/seoultech/triplanner/RegisterActivity.java
+++ app/src/main/java/com/seoultech/triplanner/RegisterActivity.java
@@ -83,60 +83,66 @@ public class RegisterActivity extends AppCompatActivity {
                 // Firebase Auth 인증 처리 절차 진행
                 mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPW)
                         .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
 
-                        // 가입 성공한 경우
-                        if (task.isSuccessful()) {
-                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
-                            UserAccount userAccount = new UserAccount();
-                            userAccount.setFbEmail(firebaseUser.getEmail());
-                            userAccount.setFbPassword(strPW);
-                            //userAccount. username 데이터 입력 필요
-                            userAccount.setImageurl("https://firebasestorage.googleapis.com/v0/b/instagram-72e36.appspot.com/o/toolbar_8.jpg?alt=media&token=83118f66-19ca-4c28-975a-b276093be5dc");
-                            userAccount.setFbIdToken(firebaseUser.getUid());  // 고유값
+                                // 가입 성공한 경우
+                                if (task.isSuccessful()) {
+                                    FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                                    UserAccount userAccount = new UserAccount();
+                                    userAccount.setFbEmail(firebaseUser.getEmail());
+                                    userAccount.setFbPassword(strPW);
+                                    //userAccount. username 데이터 입력 필요
+                                    userAccount.setImageurl("https://firebasestorage.googleapis.com/v0/b/instagram-72e36.appspot.com/o/toolbar_8.jpg?alt=media&token=83118f66-19ca-4c28-975a-b276093be5dc");
+                                    userAccount.setFbIdToken(firebaseUser.getUid());  // 고유값
 
-                            // setValue: database 에 insert 하는 행위
-                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(userAccount);
-                            Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다!", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(RegisterActivity.this, "회원가입에 실패하셨습니다!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                                    // setValue: database 에 insert 하는 행위
+                                    mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(userAccount);
+                                    Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "회원가입에 실패하셨습니다!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
 
-    private void register (String username, String email, String password){
-        mFirebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
-                            String userid = firebaseUser.getUid();
+            private void register(String username, String email, String password) {
+                mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                                    String userid = firebaseUser.getUid();
 
-                            //mDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Users").child(userid);
+                                    //mDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Users").child(userid);
 
-                            HashMap<String, Object> hashMap = new HashMap<>();
-                            hashMap.put("id", userid);
-                            hashMap.put("username", username.toLowerCase());
-                            hashMap.put("bio", "");
-                            hashMap.put("imageurl", "https://firebasestorage.googleapis.com/v0/b/instagram-72e36.appspot.com/o/toolbar_8.jpg?alt=media&token=83118f66-19ca-4c28-975a-b276093be5dc");
+                                    HashMap<String, Object> hashMap = new HashMap<>();
+                                    hashMap.put("id", userid);
+                                    hashMap.put("username", username.toLowerCase());
+                                    hashMap.put("bio", "");
+                                    hashMap.put("imageurl", "https://firebasestorage.googleapis.com/v0/b/instagram-72e36.appspot.com/o/toolbar_8.jpg?alt=media&token=83118f66-19ca-4c28-975a-b276093be5dc");
 
-                            mDatabaseRef.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()){
-                                        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                        startActivity(intent);
-                                    }
+                                    mDatabaseRef.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                startActivity(intent);
+                                            }
+                                        }
+                                    });
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "해당 이메일로 가입할 수 없습니다.", Toast.LENGTH_SHORT).show();
                                 }
-                            });
-                        } else {
-                            Toast.makeText(RegisterActivity.this, "해당 이메일로 가입할 수 없습니다.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                            }
+                        });
+
+            }
+        });
     }
 }
+
+
+    
