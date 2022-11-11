4db6106685106a505effc9d640faf81df4b7b16b
ninanoray <77103814+ninanoray@users.noreply.github.com>
2022-11-11 19:19:40 +0900

    클론코딩 MainActivity 수정 HomeFragment 생성 Planner 연결

--- app/src/main/java/com/seoultech/triplanner/Fragment/StorageFragmentLikes.java
+++ app/src/main/java/com/seoultech/triplanner/Fragment/StorageFragmentLikes.java
@@ -1,4 +1,4 @@
-package com.seoultech.triplanner;
+package com.seoultech.triplanner.Fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.seoultech.triplanner.R;
 
 public class StorageFragmentLikes extends Fragment {
 
