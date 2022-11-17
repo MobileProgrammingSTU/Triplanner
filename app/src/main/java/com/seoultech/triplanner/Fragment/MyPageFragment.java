d4c2e677bb0d81ce995eec361795a02f9bced91e
Moojun <mgim32137@gmail.com>
2022-11-18 00:59:56 +0900

    feat: MyPage에서 회원탈퇴 처리 구현 - Firebase에서 Authentication 및 Realtime db에서 내역이 지워지도록 함.
    #13

--- app/src/main/java/com/seoultech/triplanner/Fragment/MyPageFragment.java
+++ app/src/main/java/com/seoultech/triplanner/Fragment/MyPageFragment.java
@@ -16,6 +16,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.seoultech.triplanner.LoginActivity;
 import com.seoultech.triplanner.R;
 
@@ -26,7 +28,9 @@ public class MyPageFragment extends Fragment {
     ImageView myPage_profile;
     TextView myPage_userName, myPage_regDate;
     Button btn_logout, btn_quit;
+
     private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mDatabaseRef;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,6 +46,7 @@ public class MyPageFragment extends Fragment {
         btn_quit = (Button) view.findViewById(R.id.btn_quit);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Triplanner");
 
         setViewPager();
 
@@ -54,7 +59,7 @@ public class MyPageFragment extends Fragment {
             public void onClick(View v) {
                 mFirebaseAuth.signOut();    // Logout
 
-                Toast.makeText(getActivity().getApplicationContext(),
+                Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(),
                         "정상적으로 로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
 
                 Intent intent = new Intent(getActivity(), LoginActivity.class);
@@ -65,25 +70,38 @@ public class MyPageFragment extends Fragment {
         btn_quit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //showQuitDialog();
+                showQuitDialog();
             }
         });
     }
 
-    private void showQuitDialog() {         // 회원탈퇴 전 AlertDialog 창을 띄워서 다시 한 번 묻는다.
-        AlertDialog.Builder dlg = new AlertDialog.Builder(getActivity());
+    private void showQuitDialog() {// 회원탈퇴 전 AlertDialog 창을 띄워서 다시 한 번 묻는다.
+
+        final String fbCurrentUserUID = mFirebaseAuth.getUid();
+
+        AlertDialog.Builder dlg = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
         dlg.setTitle("회원 탈퇴");
         dlg.setMessage("정말로 탈퇴하시겠습니까?");
         dlg.setPositiveButton("예", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
-                // null 방지를 위해 사용
+                // Firebase Authentication 에서는 제거되나, Realtime db 에서는 제거되지 않음.
                 Objects.requireNonNull(mFirebaseAuth.getCurrentUser()).delete();
+
+                // Realtime db 에서도 제거
+                assert fbCurrentUserUID != null;
+                mDatabaseRef.child("UserAccount").child(fbCurrentUserUID).removeValue();
+
+                Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(),
+                        "정상적으로 탈퇴 되었습니다.", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
             }
         });
         dlg.setNegativeButton("아니오", null);
+        dlg.show();
     }
-
 
 }
